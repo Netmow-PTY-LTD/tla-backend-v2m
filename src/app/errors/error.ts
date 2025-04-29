9169996ce7d97ea8ed481e39cf0a39ffaddb0043
rabby <rabby.netmow@gmail.com>
2025-04-29 09:56:29 +0600

    modified coustom error in single file

---
+++ src/app/errors/error.ts
@@ -0,0 +1,107 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import mongoose from 'mongoose';
+import { TErrorSources, TGenericErrorResponse } from '../interface/error';
+import { ZodError, ZodIssue } from 'zod';
+
+class AppError extends Error {
+  public statusCode: number;
+
+  constructor(statusCode: number, message: string, stack = '') {
+    super(message);
+    this.statusCode = statusCode;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+const handleCastError = (
+  err: mongoose.Error.CastError,
+): TGenericErrorResponse => {
+  const errorSources: TErrorSources = [
+    {
+      path: err.path,
+      message: err.message,
+    },
+  ];
+
+  const statusCode = 400;
+
+  return {
+    statusCode,
+    message: 'Invalid ID',
+    errorSources,
+  };
+};
+
+const handleDuplicateError = (err: any): TGenericErrorResponse => {
+  // Extract value within double quotes using regex
+  const match = err.message.match(/"([^"]*)"/);
+
+  // The extracted value will be in the first capturing group
+  const extractedMessage = match && match[1];
+
+  const errorSources: TErrorSources = [
+    {
+      path: '',
+      message: `${extractedMessage} is already exists`,
+    },
+  ];
+
+  const statusCode = 400;
+
+  return {
+    statusCode,
+    message: 'Invalid ID',
+    errorSources,
+  };
+};
+
+const handleValidationError = (
+  err: mongoose.Error.ValidationError,
+): TGenericErrorResponse => {
+  const errorSources: TErrorSources = Object.values(err.errors).map(
+    (val: mongoose.Error.ValidatorError | mongoose.Error.CastError) => {
+      return {
+        path: val?.path,
+        message: val?.message,
+      };
+    },
+  );
+
+  const statusCode = 400;
+
+  return {
+    statusCode,
+    message: 'Validation Error',
+    errorSources,
+  };
+};
+
+const handleZodError = (err: ZodError): TGenericErrorResponse => {
+  const errorSources: TErrorSources = err.issues.map((issue: ZodIssue) => {
+    return {
+      path: issue?.path[issue.path.length - 1],
+      message: issue.message,
+    };
+  });
+
+  const statusCode = 400;
+
+  return {
+    statusCode,
+    message: 'Validation Error',
+    errorSources,
+  };
+};
+
+export const error = {
+  AppError,
+  handleCastError,
+  handleDuplicateError,
+  handleValidationError,
+  handleZodError,
+};
