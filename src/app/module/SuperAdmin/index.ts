ffd1fdc24c1f87432f24640481fabc24661278f6
rabby <rabby.netmow@gmail.com>
2025-04-30 15:18:54 +0600

    add super admin whole crud end point add in router

--- src/app/module/SuperAdmin/index.ts
+++ src/app/module/SuperAdmin/index.ts
@@ -2,6 +2,11 @@ import { Router } from 'express';
 
 import { serviceRouter } from './Service/service.route';
 import { countryRouter } from './Country/country.route';
+import { stepsCountryWiseOptionGroupsMapRouter } from './StepsCountryWiseOptionGroupsMap/stepsCountryWiseOptionGroupsMap.route';
+import { CountryWiseServiceMapRouter } from './CountryWiseServiceMap/countryWiseServiceMap.route';
+import { countryStepsOptionMapRouter } from './CountryStepsOptionMap/countryStepsOptionMap.route';
+import { OptionGroupRouter } from './OptionGroup/optionGroup.route';
+import { OptionRouter } from './Option/option.route';
 
 const superAdminRoute = Router();
 
@@ -13,6 +18,26 @@ const moduleRoutes = [
   {
     path: '/country',
     route: countryRouter,
+  },
+  {
+    path: '/steps_country_wise_service_wise_option_groups_map',
+    route: stepsCountryWiseOptionGroupsMapRouter,
+  },
+  {
+    path: '/country_wise_service_map',
+    route: CountryWiseServiceMapRouter,
+  },
+  {
+    path: '/country_steps_option_group-option-map',
+    route: countryStepsOptionMapRouter,
+  },
+  {
+    path: '/option_groups',
+    route: OptionGroupRouter,
+  },
+  {
+    path: '/options',
+    route: OptionRouter,
   },
 ];
 
