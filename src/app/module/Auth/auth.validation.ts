664dfa66cdccceccf9d67e5d60c9e5267442043a
rabby <rabby.netmow@gmail.com>
2025-05-09 16:10:25 +0600

    added change password api

--- src/app/module/Auth/auth.validation.ts
+++ src/app/module/Auth/auth.validation.ts
@@ -32,8 +32,18 @@ const refreshTokenValidationSchema = z.object({
   }),
 });
 
+const changePasswordValidationSchema = z.object({
+  body: z.object({
+    oldPassword: z.string({
+      required_error: 'Old password is required',
+    }),
+    newPassword: z.string({ required_error: 'Password is required' }),
+  }),
+});
+
 export const authZodValidation = {
   userZodValidationSchema,
   refreshTokenValidationSchema,
   loginValidationSchema,
+  changePasswordValidationSchema,
 };
