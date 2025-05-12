7356b2d884d95e81cbb1ff1f569aa7e31c7d435e
rabby <rabby.netmow@gmail.com>
2025-05-12 12:53:53 +0600

    fixed valitdation

--- src/app/module/User/user.validation.ts
+++ src/app/module/User/user.validation.ts
@@ -1,19 +1,26 @@
 import { z } from 'zod';
-import mongoose from 'mongoose';
+import { USER_PROFILE } from './user.constant';
+
+// Zod enum from USER_PROFILE values
+const userProfileEnum = z.enum([
+  USER_PROFILE.BASIC,
+  USER_PROFILE.PREMIUM,
+  USER_PROFILE.ADMIN,
+]);
 
 const userUpdateZodValidationSchema = z.object({
   body: z.object({
+    // user: z.string().min(1, 'User ID is required'), // ObjectId as string
     firstName: z.string().min(1, 'First name is required'),
     lastName: z.string().min(1, 'Last name is required'),
-    email: z.string().email('Invalid email address'),
-    role: z.string().min(1, 'Role is required'),
-    password: z.string().min(6, 'Password must be at least 6 characters'),
-    country: z
-      .string()
-      .refine((val) => mongoose.Types.ObjectId.isValid(val), {
-        message: 'Invalid country ObjectId',
-      })
-      .optional(),
+    activeProfile: userProfileEnum.optional(),
+    country: z.string().optional(),
+    // country: z
+    //   .string()
+    //   .refine((val) => mongoose.Types.ObjectId.isValid(val), {
+    //     message: 'Invalid country ObjectId',
+    //   })
+    //   .optional(),
   }),
 });
 
