94739eb0153c583ad99784b1eae6941cdecfc395
rabby <rabby.netmow@gmail.com>
2025-05-14 13:11:34 +0600

    fixed country wise service module

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -5,10 +5,11 @@ import { UserProfileRouter } from '../module/User/routes/user.route';
 import { countryRouter } from '../module/geo/Country/routes/country.route';
 import { serviceRouter } from '../module/service/Service/routes/service.route';
 import { stepsCountryWiseOptionGroupsMapRouter } from '../module/service/StepsCountryWiseOptionGroupsMap/routes/stepsCountryWiseOptionGroupsMap.route';
-import { CountryWiseServiceMapRouter } from '../module/geo/CountryWiseServiceMap/routes/countryWiseServiceMap.route';
+
 import { countryStepsOptionMapRouter } from '../module/geo/CountryStepsOptionMap/routes/countryStepsOptionMap.route';
 import { OptionGroupRouter } from '../module/service/OptionGroup/routes/optionGroup.route';
 import { OptionRouter } from '../module/service/Option/routes/option.route';
+import { CountryWiseServiceRouter } from '../module/geo/CountryWiseServices/routes/countryWiseServiceMap.route';
 
 const router = Router();
 
@@ -34,8 +35,8 @@ const moduleRoutes = [
     route: stepsCountryWiseOptionGroupsMapRouter,
   },
   {
-    path: '/country_wise_service_map',
-    route: CountryWiseServiceMapRouter,
+    path: '/country_wise_service',
+    route: CountryWiseServiceRouter,
   },
   {
     path: '/country_steps_option_group-option-map',
