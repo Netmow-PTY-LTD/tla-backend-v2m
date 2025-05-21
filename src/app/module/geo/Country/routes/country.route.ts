5719806aa2de6e6908e0fde454d25d507822cab5
rabby <rabby.netmow@gmail.com>
2025-05-21 11:59:25 +0600

    added zip code crud api

--- src/app/module/geo/Country/routes/country.route.ts
+++ src/app/module/geo/Country/routes/country.route.ts
@@ -4,9 +4,12 @@ import { countryZodValidation } from '../validations/country.validation';
 import { countryController } from '../controllers/country.controller';
 import auth from '../../../../middlewares/auth';
 import { USER_ROLE } from '../../../../constant';
+import { zipcodeZodValidation } from '../validations/zipcode.validation';
+import { zipCodeController } from '../controllers/zipcode.controller';
 
 const router = Router();
 
+// country dedicated end point
 router.post(
   '/add',
   auth(USER_ROLE.ADMIN),
@@ -30,6 +33,36 @@ router.patch(
   auth(USER_ROLE.ADMIN),
   validateRequest(countryZodValidation.updateCountryZodValidationSchema),
   countryController.updateSingleCountry,
+);
+
+//  zip code dedicated end point
+router.post(
+  '/zipcode/add',
+  auth(USER_ROLE.ADMIN),
+  validateRequest(zipcodeZodValidation.zipcodeZodValidationSchema),
+  zipCodeController.createZipCode,
+);
+router.get(
+  '/zipcode/list',
+  auth(USER_ROLE.ADMIN),
+  zipCodeController.getAllZipCode,
+);
+router.get(
+  '/zipcode/:zipcodeId',
+  auth(USER_ROLE.ADMIN),
+  zipCodeController.getSingleZipCode,
+);
+router.delete(
+  '/zipcode/delete/:zipcodeId',
+  auth(USER_ROLE.ADMIN),
+
+  zipCodeController.deleteSingleZipCode,
+);
+router.patch(
+  '/zipcode/edit/:zipcodeId',
+  auth(USER_ROLE.ADMIN),
+  validateRequest(zipcodeZodValidation.updateZipcodeZodValidationSchema),
+  zipCodeController.updateSingleZipCode,
 );
 
 export const countryRouter = router;
---
+++ src/app/module/geo/Country/services/zipcode.service.ts
@@ -0,0 +1,53 @@
+import { validateObjectId } from '../../../../utils/validateObjectId';
+import { IZipCode } from '../interfaces/zipcode.interface';
+import ZipCode from '../models/zipcode.model';
+
+const CreateZipCodeIntoDB = async (payload: IZipCode) => {
+  const zipCode = await ZipCode.create(payload);
+  return zipCode;
+};
+
+const getAllZipCodeFromDB = async () => {
+  const countries = await ZipCode.find({ deletedAt: null });
+  return countries;
+};
+
+const getSingleZipCodeFromDB = async (id: string) => {
+  validateObjectId(id, 'ZipCode');
+  const result = await ZipCode.findOne({ _id: id, deletedAt: null });
+  return result;
+};
+
+const updateZipCodeIntoDB = async (id: string, payload: Partial<IZipCode>) => {
+  validateObjectId(id, 'ZipCode');
+  const result = await ZipCode.findOneAndUpdate(
+    { _id: id, deletedAt: null },
+    payload,
+    {
+      new: true,
+    },
+  );
+  return result;
+};
+
+const deleteZipCodeFromDB = async (id: string) => {
+  validateObjectId(id, 'ZipCode');
+  const deletedAt = new Date().toISOString();
+
+  const result = await ZipCode.findByIdAndUpdate(
+    id,
+    { deletedAt: deletedAt },
+    {
+      new: true,
+    },
+  );
+  return result;
+};
+
+export const zipCodeService = {
+  CreateZipCodeIntoDB,
+  getAllZipCodeFromDB,
+  getSingleZipCodeFromDB,
+  updateZipCodeIntoDB,
+  deleteZipCodeFromDB,
+};
