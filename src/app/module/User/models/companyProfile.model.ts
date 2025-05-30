f6f694898234bde9e736298e4286c0d8ae44223c
rabby <rabby.netmow@gmail.com>
2025-05-30 18:13:47 +0600

    fixed some enum issues

--- src/app/module/User/models/companyProfile.model.ts
+++ src/app/module/User/models/companyProfile.model.ts
@@ -51,20 +51,18 @@ const companyProfileSchema = new mongoose.Schema(
       },
       locationReason: {
         type: String,
-        enum: ['No Location', 'Online only', 'Multiple Location'],
+        enum: ['no_location', 'online_only', 'multiple_location'],
       },
     },
     companySize: {
       type: String,
-      // enum: [
-      //   'Self-employed / sole trader',
-      //   '2–10 employees',
-      //   '11–50 employees',
-      //   '51–200 employees',
-      //   '51–200 employees',
-      //   'Over 200 employees',
-      // ],
-      // required: true,
+      enum: [
+        'self_employed',
+        '2_10_employees',
+        '11_50_employees',
+        '51_200_employees',
+        'over_200_employees',
+      ],
     },
     yearsInBusiness: {
       type: Number,
