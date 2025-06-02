f28d310edfc5e762bed05bbfb520d4634392951e
rabby <rabby.netmow@gmail.com>
2025-06-02 16:30:21 +0600

    delete unused code

--- src/app/module/User/services/profileAccreditation.service.ts
+++ src/app/module/User/services/profileAccreditation.service.ts
@@ -9,52 +9,6 @@ import { IAccreditation } from '../interfaces/profileAccreditation';
 import Accreditation from '../models/ProfileAccreditation';
 import UserProfile from '../models/user.model';
 
-// const updateProfileAccreditationIntoDB = async (
-//   id: string,
-//   payload: Partial<IAccreditation>,
-//   file?: TUploadedFile,
-// ) => {
-//   // Check if the user exists in the database by ID
-//   const userProfile = await UserProfile.findOne({ user: id });
-
-//   if (!userProfile) {
-//     // Return early if userProfile is not found — no error
-//     return sendNotFoundResponse('user profile data');
-//   }
-
-//   // ✅ Handle file upload if provided
-//   if (file?.buffer) {
-//     try {
-//       const uploadedUrl = await uploadToSpaces(
-//         file.buffer,
-//         file.originalname,
-//         id,
-//       );
-//       payload.attachment = uploadedUrl;
-//       // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
-//     } catch (err) {
-//       throw new AppError(
-//         HTTP_STATUS.INTERNAL_SERVER_ERROR,
-//         'File upload failed',
-//       );
-//     }
-//   }
-
-//   // Update the company  profile in the database
-
-//   const updateAccreditation = await Accreditation.findOneAndUpdate(
-//     { userProfileId: userProfile._id },
-//     payload,
-//     {
-//       upsert: true,
-//       new: true,
-//     },
-//   );
-
-//   // Return the updated profile
-//   return updateAccreditation;
-// };
-
 const updateProfileAccreditationIntoDB = async (
   id: string,
   payload: Partial<IAccreditation> & { _id?: string },
