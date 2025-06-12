5169274822a47cfebf9503bca79a7af2d9049a6c
rabby <rabby.netmow@gmail.com>
2025-06-12 15:02:09 +0600

    fixed range validation issues

--- src/app/module/geo/Country/validations/range.validation.ts
+++ src/app/module/geo/Country/validations/range.validation.ts
@@ -1,10 +1,10 @@
 import { z } from 'zod';
-import { zodObjectIdField } from '../../../../utils/validateObjectId';
+// import { zodObjectIdField } from '../../../../utils/validateObjectId';
 
 const rangeZodValidationSchema = z.object({
   body: z.object({
     // countryId: zodObjectIdField('country'),
-    zipCodeId: zodObjectIdField('zip code'),
+    // zipCodeId: zodObjectIdField('zip code'),
     name: z.string().min(2, 'Name too short').max(50, 'Name too long'),
     value: z.number().min(0, 'Range must be a positive number'),
     unit: z.enum(['km', 'miles']).default('km'),
