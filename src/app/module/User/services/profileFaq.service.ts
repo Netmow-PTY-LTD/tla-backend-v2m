e184d227c51fe90be3b8b1ce66cba10865a56637
rabby <rabby.netmow@gmail.com>
2025-06-23 16:50:03 +0600

    added faq update and create api

---
+++ src/app/module/User/services/profileFaq.service.ts
@@ -0,0 +1,42 @@
+import { sendNotFoundResponse } from '../../../errors/custom.error';
+import Faq, { IFaq } from '../models/faq.model';
+import UserProfile from '../models/user.model';
+
+const updateProfileFaqIntoDB = async (
+  userId: string,
+  payload: Partial<IFaq> & { _id?: string }, // accept _id like the other function
+) => {
+  const userProfile = await UserProfile.findOne({ user: userId });
+
+  if (!userProfile) {
+    return sendNotFoundResponse('user profile data');
+  }
+
+  let faq;
+
+  // Try to update existing FAQ if _id exists
+  if (payload._id) {
+    faq = await Faq.findByIdAndUpdate(
+      payload._id,
+      {
+        ...payload,
+        userProfileId: userProfile._id,
+      },
+      { new: true },
+    );
+  }
+
+  // If not found or no _id, create new FAQ
+  if (!faq) {
+    faq = await Faq.create({
+      ...payload,
+      userProfileId: userProfile._id,
+    });
+  }
+
+  return faq;
+};
+
+export const profileFaqService = {
+  updateProfileFaqIntoDB,
+};
