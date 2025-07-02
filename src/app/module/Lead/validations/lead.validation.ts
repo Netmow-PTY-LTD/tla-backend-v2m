492f5b56b12d47e8d129519c0622214384bda94c
rabby <rabby.netmow@gmail.com>
2025-07-02 15:12:37 -0700

    change logic get single response by responseid

--- src/app/module/Lead/validations/lead.validation.ts
+++ src/app/module/Lead/validations/lead.validation.ts
@@ -4,19 +4,32 @@ import { zodObjectIdField } from '../../../utils/validateObjectId';
 const leadZodValidationSchema = z.object({
   body: z.object({
     userProfileId: zodObjectIdField('user profile '),
-    service_id: zodObjectIdField('service'),
+    serviceId: zodObjectIdField('service'),
+    locationId: zodObjectIdField('location'),
+    additionalDetails: z.string().optional().default(''),
+    budgetAmount: z.number().min(0).default(0),
   }),
 });
 
 const updateLeadZodValidationSchema = z.object({
   body: z.object({
-    userProfileId: zodObjectIdField('user profile ').optional(),
-    service_id: zodObjectIdField('service').optional(),
+    userProfileId: zodObjectIdField('user profile '),
+    serviceId: zodObjectIdField('service'),
+    locationId: zodObjectIdField('location'),
+    additionalDetails: z.string().optional().default(''),
+    budgetAmount: z.number().min(0).default(0),
   }),
 });
+
 
 // Exporting all validation schemas
 export const leadZodValidation = {
   leadZodValidationSchema,
   updateLeadZodValidationSchema,
 };
+
+
+
+
+
+
