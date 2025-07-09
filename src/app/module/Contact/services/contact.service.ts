3e5e9d946eca2bf2cce06d3de852f1ee23311e82
rabby <rabby.netmow@gmail.com>
2025-07-09 03:29:56 -0700

    fixed issues

--- src/app/module/Contact/services/contact.service.ts
+++ src/app/module/Contact/services/contact.service.ts
@@ -1,113 +1,4 @@
 
-// import { sendSMS } from '../../../config/smsTransporter';
-// import { sendEmail } from '../../../emails/email.service';
-// import { sendNotFoundResponse } from '../../../errors/custom.error';
-// import UserProfile from '../../User/models/user.model';
-// import { SendEmail } from '../models/SendEmail.model';
-// import { SendSMS } from '../models/SendSMS.model';
-
-// const sendContactMessage = async (
-//   userId: string,
-//   payload: {
-//     toEmail?: string;
-//     toPhone?: string;
-//     subject: string;
-//     emailText: string;
-//     message: string;
-//     leadId?: string;
-//     responseId?: string;
-//     method: 'email' | 'sms';
-//   }
-// ) => {
-
-//   const user = await UserProfile.findOne({ user: userId }).select('_id');
-
-//   if (!user) {
-//     return sendNotFoundResponse('User profile not found');
-//   }
-
-//   const { toEmail, toPhone, subject, message, leadId, method, responseId, emailText } = payload;
-
-//   if (method === 'email' && toEmail) {
-//     try {
-//       // neeed html format
-//       const html = emailText
-//       const result = await sendEmail({ to: toEmail, subject, html });
-
-//       await SendEmail.create({
-//         to: toEmail,
-//         subject,
-//         html,
-//         sentBy: user?._id,
-//         leadId,
-//         responseId,
-//         status: 'sent',
-//       });
-
-
-//       return { message: 'Email sent successfully', data: result };
-//     } catch (error: any) {
-//       // neeed html format
-//       const html = emailText
-//       await SendEmail.create({
-//         to: toEmail,
-//         subject,
-//         html,
-//         sentBy: user?._id,
-//         leadId,
-//         responseId,
-//         status: 'failed',
-//         error: error.message,
-//       });
-
-//       throw new Error(`Failed to send email: ${error.message}`);
-//     }
-//   }
-
-//   if (method === 'sms' && toPhone) {
-//     try {
-//       const result = await sendSMS({ to: toPhone, message });
-
-//       await SendSMS.create({
-//         to: toPhone,
-//         message,
-//         sentBy: user?._id,
-//         leadId,
-//         responseId,
-//         status: 'sent',
-//         provider: 'twilio',
-//         metadata: result,
-//       });
-
-//       return { message: 'SMS sent successfully', data: result };
-//     } catch (error: any) {
-//       await SendSMS.create({
-//         to: toPhone,
-//         message,
-//         sentBy: user?._id,
-//         leadId,
-//         responseId,
-//         status: 'failed',
-//         provider: 'twilio',
-//         error: error.message,
-//       });
-
-//       throw new Error(`Failed to send SMS: ${error.message}`);
-//     }
-//   }
-
-//   throw new Error('Invalid method or missing contact info');
-// };
-
-
-
-// export const contactservice = {
-//   sendContactMessage
-// };
-
-
-
-
 
 import { sendSMS } from '../../../config/smsTransporter';
 import { sendEmail } from '../../../emails/email.service';
@@ -152,7 +43,7 @@ const sendContactMessage = async (
 
   if (method === 'email' && toEmail) {
     try {
-      const html = emailText;
+      const html = `<p>${emailText}</p>`;
       const result = await sendEmail({ to: toEmail, subject, html });
 
       const resultDB = await SendEmail.create({
@@ -167,7 +58,7 @@ const sendContactMessage = async (
       });
 
       await logActivity({
-        createdBy: user._id,
+        createdBy: userId,
         activityNote: `Sent email to ${toEmail}`,
         activityType: 'sendemail',
         module: 'response',
@@ -182,7 +73,7 @@ const sendContactMessage = async (
 
       return { message: 'Email sent successfully', data: resultDB };
     } catch (error: any) {
-      const html = emailText;
+      const html = `<p>${emailText}</p>`;
       await SendEmail.create({
         to: toEmail,
         subject,
@@ -196,7 +87,7 @@ const sendContactMessage = async (
       });
 
       await logActivity({
-        createdBy: user._id,
+        createdBy: userId,
         activityNote: `Failed to send email to ${toEmail}`,
         activityType: 'sendemail',
         module: 'response',
@@ -230,7 +121,7 @@ const sendContactMessage = async (
       });
 
       await logActivity({
-        createdBy: user._id,
+        createdBy: userId,
         activityNote: `Sent SMS to ${toPhone}`,
         activityType: 'sendsms',
         module: 'response',
@@ -256,7 +147,7 @@ const sendContactMessage = async (
       });
 
       await logActivity({
-        createdBy: user._id,
+        createdBy: userId,
         activityNote: `Failed to send SMS to ${toPhone}`,
         activityType: 'sendsms',
         module: 'response',
