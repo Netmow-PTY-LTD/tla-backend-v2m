71e2806782e79b2fa07e9505dce88a667bee380e
rabby <rabby.netmow@gmail.com>
2025-07-15 13:03:21 +0600

    create app settings full crud

---
+++ src/app/module/Settings/routes/settings.route.ts
@@ -0,0 +1,23 @@
+
+import { Router } from 'express';
+
+
+
+import auth from '../../../middlewares/auth';
+import { USER_ROLE } from '../../../constant';
+import { settingsController } from '../controllers/settings.controller';
+
+const router = Router();
+
+router.get('/',
+    // auth(USER_ROLE.ADMIN),
+    settingsController.getAppSettings);
+router.patch('/',
+    // auth(USER_ROLE.ADMIN),
+    settingsController.updateAppSettings);
+router.post('/reset',
+    // auth(USER_ROLE.ADMIN),
+    settingsController.resetAppSettings);
+
+
+export const settingsRouter = router;
---
+++ src/app/module/Settings/services/settings.service.ts
@@ -0,0 +1,31 @@
+
+
+import { IAppSettings } from "../interfaces/settings.interface";
+import { AppSettings } from "../models/settings.model";
+
+
+ const getSettings = async () => {
+  let settings = await AppSettings.findOne();
+  if (!settings) {
+    settings = await AppSettings.create({});
+  }
+  return settings;
+};
+
+ const updateSettings = async (updates: Partial<IAppSettings>) => {
+  const settings = await getSettings();
+  Object.assign(settings, updates);
+  return settings.save();
+};
+
+ const resetSettings = async () => {
+  await AppSettings.deleteMany({});
+  return AppSettings.create({});
+};
+
+
+export  const settingsService={
+getSettings,
+updateSettings,
+resetSettings
+}
