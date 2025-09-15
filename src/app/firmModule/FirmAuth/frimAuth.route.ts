2e786230c63dbdf226ada413bf9a635d2f2a80a0
rabby <rabby.netmow@gmail.com>
2025-09-15 14:49:29 +0600

    create firm staff register api

--- src/app/firmModule/FirmAuth/frimAuth.route.ts
+++ src/app/firmModule/FirmAuth/frimAuth.route.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
+import { firmAuthController } from "./frimAuth.controller";
 
 
 
 const router = Router();
 
 
-router.post('/register/staff')
+router.post('/register/staff', firmAuthController.staffRegister)
 
 
 
