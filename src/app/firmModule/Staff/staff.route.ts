1baeb1422690c84e780fc1e5ea68116df8412cf5
rabby <rabby.netmow@gmail.com>
2025-09-26 16:19:29 +0600

    fixed staff create list and single staff and update api

--- src/app/firmModule/Staff/staff.route.ts
+++ src/app/firmModule/Staff/staff.route.ts
@@ -5,18 +5,18 @@ import { Firm_USER_ROLE } from '../FirmAuth/frimAuth.constant';
 
 const router = Router();
 
-router.post('/add', firmAuth(Firm_USER_ROLE.FIRM), staffController.createStaff);
+router.post('/add', firmAuth(Firm_USER_ROLE.ADMIN, Firm_USER_ROLE.FIRM, Firm_USER_ROLE.STAFF), staffController.createStaff);
 
 // GET staff list
-router.get('/:firmId/list', staffController.listStaff);
+router.get('/list', firmAuth(Firm_USER_ROLE.ADMIN, Firm_USER_ROLE.FIRM, Firm_USER_ROLE.STAFF), staffController.listStaff);
 
 // GET staff by id
-router.get('/:firmId/:staffId', staffController.getStaffById);
+router.get('/:staffId', firmAuth(Firm_USER_ROLE.ADMIN, Firm_USER_ROLE.FIRM, Firm_USER_ROLE.STAFF), staffController.getStaffById);
 
 // PUT update staff
-router.put('/:firmId/:staffId/update', staffController.updateStaff);
+router.put('/:staffId/update',firmAuth(Firm_USER_ROLE.ADMIN, Firm_USER_ROLE.FIRM, Firm_USER_ROLE.STAFF), staffController.updateStaff);
 
 // DELETE staff
-router.delete('/:firmId/:staffId/delete', staffController.deleteStaff);
+router.delete('/:staffId/delete', firmAuth(Firm_USER_ROLE.ADMIN, Firm_USER_ROLE.FIRM, Firm_USER_ROLE.STAFF), staffController.deleteStaff);
 
 export const staffRoutes = router;
