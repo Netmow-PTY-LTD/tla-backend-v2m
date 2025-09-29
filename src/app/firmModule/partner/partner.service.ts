f04e620dc16d216fb6ab0eb8e5ca2bef5bcf7370
rabby <rabby.netmow@gmail.com>
2025-09-29 16:13:53 +0600

    fixed partner related crud issues

--- src/app/firmModule/partner/partner.service.ts
+++ src/app/firmModule/partner/partner.service.ts
@@ -1,36 +1,37 @@
+import { sendNotFoundResponse } from "../../errors/custom.error";
 import { FirmProfile } from "../Firm/firm.model";
+import FirmUser from "../FirmAuth/frimAuth.model";
 import { IPartner } from "./partner.interface";
 import { FirmPartner } from "./partner.model";
 
 
-
 const createPartner = async (userId: string, data: Partial<IPartner>) => {
-  // Get the firm profile ID for this user
-  const firmProfile = await FirmProfile.findOne({ userId: userId });
-  if (!firmProfile) {
-    throw new Error("Firm profile not found for this user");
-  }
+  const user = await FirmUser.findById(userId).select('firmProfileId')
 
+  if (!user) {
+    return sendNotFoundResponse("User not found");
+  }
 
   // Include firmProfileId in the license data
   const partnerData = {
     ...data,
-    firmProfileId: firmProfile._id,
+    firmProfileId: user.firmProfileId,
   };
-
 
   return await FirmPartner.create(partnerData);
 };
 
+
 const getPartnerList = async (userId: string) => {
 
-  // Get the firm profile ID for this user
-  const firmProfile = await FirmProfile.findOne({ userId: userId });
-  if (!firmProfile) {
-    throw new Error("Firm profile not found for this user");
+  const user = await FirmUser.findById(userId).select('firmProfileId')
+
+  if (!user) {
+    return sendNotFoundResponse("User not found");
   }
 
-  return await FirmPartner.find({ firmProfileId: firmProfile?._id });
+
+  return await FirmPartner.find({ firmProfileId: user.firmProfileId });
 };
 
 
