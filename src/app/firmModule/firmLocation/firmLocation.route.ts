8ae57e601581cf7160c89a8f827ab4558f84f017
rabby <rabby.netmow@gmail.com>
2025-09-29 17:53:19 +0600

    fixed issues

--- src/app/firmModule/firmLocation/firmLocation.route.ts
+++ src/app/firmModule/firmLocation/firmLocation.route.ts
@@ -8,10 +8,10 @@ import { Firm_USER_ROLE } from "../FirmAuth/frimAuth.constant";
 const router = Router();
 
 
-router.post("/add", firmAuth(Firm_USER_ROLE.ADMIN), firmLocationController.createLocation);
-router.get("/list", firmAuth(Firm_USER_ROLE.ADMIN), firmLocationController.listLocations);
-router.get("/:locationId", firmAuth(Firm_USER_ROLE.ADMIN), firmLocationController.getSingleLocation);
-router.put("/:locationId/update", firmAuth(Firm_USER_ROLE.ADMIN), firmLocationController.updateLocation);
-router.delete("/:locationId/delete", firmAuth(Firm_USER_ROLE.ADMIN), firmLocationController.deleteLocation);
+router.post("/add", firmAuth(Firm_USER_ROLE.ADMIN, Firm_USER_ROLE.STAFF), firmLocationController.createLocation);
+router.get("/list", firmAuth(Firm_USER_ROLE.ADMIN, Firm_USER_ROLE.STAFF), firmLocationController.listLocations);
+router.get("/:locationId", firmAuth(Firm_USER_ROLE.ADMIN, Firm_USER_ROLE.STAFF), firmLocationController.getSingleLocation);
+router.put("/:locationId/update", firmAuth(Firm_USER_ROLE.ADMIN, Firm_USER_ROLE.STAFF), firmLocationController.updateLocation);
+router.delete("/:locationId/delete", firmAuth(Firm_USER_ROLE.ADMIN, Firm_USER_ROLE.STAFF), firmLocationController.deleteLocation);
 
 export const firmLocationRouter= router;
