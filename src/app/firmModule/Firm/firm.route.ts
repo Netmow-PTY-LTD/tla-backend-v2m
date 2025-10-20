f3708999bcad83a8ffb27344fff2133c4b8abced
rabby <rabby.netmow@gmail.com>
2025-10-20 13:13:19 +0600

    create case stats hire un hire of specific firm lawyer

--- src/app/firmModule/Firm/firm.route.ts
+++ src/app/firmModule/Firm/firm.route.ts
@@ -8,14 +8,13 @@ const router = Router();
 
 router.get(
   '/firmInfo',
-  firmAuth(Firm_USER_ROLE.STAFF,Firm_USER_ROLE.ADMIN),
+  firmAuth(Firm_USER_ROLE.STAFF, Firm_USER_ROLE.ADMIN),
   firmController.getFirmInfo,
 );
 
-
 router.get(
   '/dashboard/stats',
-  firmAuth(Firm_USER_ROLE.STAFF,Firm_USER_ROLE.ADMIN),
+  firmAuth(Firm_USER_ROLE.STAFF, Firm_USER_ROLE.ADMIN),
   firmController.getFirmDasboardStats,
 );
 
@@ -27,20 +26,14 @@ router.put(
     req.body = JSON.parse(req.body.data);
     next();
   },
-  firmAuth(Firm_USER_ROLE.STAFF,Firm_USER_ROLE.ADMIN),
+  firmAuth(Firm_USER_ROLE.STAFF, Firm_USER_ROLE.ADMIN),
   firmController.updateFirmInfo,
 );
 
-// Admin  Firm Management Endpoints
-
-router.post('/', firmAuth(Firm_USER_ROLE.ADMIN), firmController.createFirm);
-router.get('/', firmAuth(Firm_USER_ROLE.ADMIN), firmController.listFirms);
-router.get('/:id', firmController.getFirmById);
-router.put('/:id', firmAuth(Firm_USER_ROLE.ADMIN), firmController.updateFirm);
-router.delete(
-  '/firms/:id',
-  firmAuth(Firm_USER_ROLE.ADMIN),
-  firmController.deleteFirm,
+router.get(
+  '/dashboard/firmlawyers-case/stats',
+  firmAuth(Firm_USER_ROLE.STAFF, Firm_USER_ROLE.ADMIN),
+  firmController.getFirmLawyerLeadStatsByDate,
 );
 
 
