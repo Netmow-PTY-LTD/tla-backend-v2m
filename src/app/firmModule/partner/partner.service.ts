24a71d3ccdde8caa56eea8c736b367754e1a99d0
rabby <rabby.netmow@gmail.com>
2025-10-21 17:19:28 +0600

    fixed dgo file uploding of partner

--- src/app/firmModule/partner/partner.service.ts
+++ src/app/firmModule/partner/partner.service.ts
@@ -1,16 +1,39 @@
+import { uploadToSpaces } from "../../config/upload";
+import { FOLDERS } from "../../constant";
 import { sendNotFoundResponse } from "../../errors/custom.error";
+import { TUploadedFile } from "../../interface/file.interface";
 import { FirmProfile } from "../Firm/firm.model";
 import FirmUser from "../FirmAuth/frimAuth.model";
 import { IPartner } from "./partner.interface";
 import { FirmPartner } from "./partner.model";
 
 
-const createPartner = async (userId: string, data: Partial<IPartner>) => {
+const createPartner = async (userId: string, data: Partial<IPartner>, file: TUploadedFile) => {
   const user = await FirmUser.findById(userId).select('firmProfileId')
 
   if (!user) {
     return sendNotFoundResponse("User not found");
   }
+
+
+  //  handle file upload if present
+  if (file.buffer) {
+    const fileBuffer = file.buffer;
+    const originalName = file.originalname;
+
+    // upload to Spaces and get public URL
+    // const logoUrl = await uploadToSpaces(fileBuffer, originalName, userId);
+
+    const logoUrl = await uploadToSpaces(fileBuffer, originalName, {
+      folder: FOLDERS.FIRMS,
+      entityId: `partner-${user.firmProfileId}`,
+      subFolder: FOLDERS.PROFILES
+    });
+    data.image = logoUrl;
+
+  }
+
+
 
   // Include firmProfileId in the license data
   const partnerData = {
@@ -37,14 +60,47 @@ const getPartnerList = async (userId: string) => {
 
 
 const updatePartner = async (
+  userId: string,
   partnerId: string,
-  data: Partial<IPartner>
+  data: Partial<IPartner>,
+  file: TUploadedFile
 ) => {
+
+
+  const user = await FirmUser.findById(userId).select('firmProfileId')
+
+  if (!user) {
+    return sendNotFoundResponse("User not found");
+  }
+
+  
+
+  //  handle file upload if present
+  if (file.buffer) {
+    const fileBuffer = file.buffer;
+    const originalName = file.originalname;
+    // upload to Spaces and get public URL
+    // const logoUrl = await uploadToSpaces(fileBuffer, originalName, userId);
+
+    const logoUrl = await uploadToSpaces(fileBuffer, originalName, {
+      folder: FOLDERS.FIRMS,
+      entityId: `partner-${user.firmProfileId}`,
+      subFolder: FOLDERS.PROFILES
+    });
+    data.image = logoUrl;
+
+  }
+
+
+
   return await FirmPartner.findOneAndUpdate(
     { _id: partnerId },
     data,
     { new: true }
   );
+
+
+
 };
 
 const deletePartner = async (partnerId: string) => {
