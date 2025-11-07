39df23b6f0c4e23acbecb2762253febbe57206b2
rabby <rabby.netmow@gmail.com>
2025-11-07 09:38:51 +0600

    added robots field for search engine visiblity

--- src/app/module/Settings/settings.model.ts
+++ src/app/module/Settings/settings.model.ts
@@ -14,6 +14,7 @@ const settingsSchema = new Schema(
     responseLimitPerLead: { type: Number, default: 5 },
     stripeLiveMode: { type: Boolean, default: false },
     autoRefundIfLeadInactive: { type: Boolean, default: true },
+    robots: { type: String, default: 'noindex, nofollow' },
   },
   { timestamps: true },
 );
