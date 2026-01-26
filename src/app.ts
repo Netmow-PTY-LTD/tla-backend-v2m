0a7e04e7cb4f96cb29def6e98ee7bece76753c68
rabby <rabby.netmow@gmail.com>
2026-01-26 15:02:55 +0600

    fixed isusues of version

--- src/app.ts
+++ src/app.ts
@@ -99,7 +99,7 @@ app.get('/online-users', async (_req: Request, res: Response) => {
 
 
 app.get('/', (_req: Request, res: Response) => {
-  res.send('Welcome to TLA Backend  2.0');
+  res.send('Welcome to TLA Backend  2.01');
 });
 
 app.use(globalErrorHandler);
