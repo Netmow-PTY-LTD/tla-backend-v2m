fb5965136712b95a7056dcb5a31756ffc82021d7
rabby <rabby.netmow@gmail.com>
2026-02-17 16:14:24 +0600

    fixed issues of tax amount

--- src/app/module/Country/country.model.ts
+++ src/app/module/Country/country.model.ts
@@ -58,6 +58,37 @@ const countriesSchema = new mongoose.Schema(
   },
 );
 
+countriesSchema.pre('save', function (next) {
+  if (this.taxPercentage && this.taxPercentage > 0) {
+    this.taxAmount = 0;
+  } else if (this.taxAmount && this.taxAmount > 0) {
+    this.taxPercentage = 0;
+  }
+  next();
+});
+
+countriesSchema.pre('findOneAndUpdate', function (next) {
+  const update = this.getUpdate() as mongoose.UpdateQuery<ICountry>;
+  if (!update) return next();
+
+  if (update.taxPercentage && (update.taxPercentage as number) > 0) {
+    update.taxAmount = 0;
+  } else if (update.taxAmount && (update.taxAmount as number) > 0) {
+    update.taxPercentage = 0;
+  }
+
+  // Handle $set if present
+  if (update.$set) {
+    if (update.$set.taxPercentage && (update.$set.taxPercentage as number) > 0) {
+      update.$set.taxAmount = 0;
+    } else if (update.$set.taxAmount && (update.$set.taxAmount as number) > 0) {
+      update.$set.taxPercentage = 0;
+    }
+  }
+
+  next();
+});
+
 countriesSchema.statics.isCountryExists = async function (id: string) {
   return await Country.findById(id);
 };
