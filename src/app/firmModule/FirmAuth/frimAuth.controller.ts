bb52b36a0cf08eb10cecb7109fe18182a9c1b183
rabby <rabby.netmow@gmail.com>
2026-02-20 14:48:00 +0600

    fixed issues

--- src/app/firmModule/FirmAuth/frimAuth.controller.ts
+++ src/app/firmModule/FirmAuth/frimAuth.controller.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import config from "../../config";
 import { HTTP_STATUS } from "../../constant/httpStatus";
 import { AppError } from "../../errors/error";
 import { TUploadedFile } from "../../interface/file.interface";
